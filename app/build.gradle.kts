764541c122be9546cbd7283f983a09e914464b77
8954sood <58807462+8954sood@users.noreply.github.com>
2024-01-06 23:05:34 +0900

    Create: Chat View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,9 @@ dependencies {
     // lottie
     implementation("com.airbnb.android:lottie:6.1.0")
 
+    // cricleImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
